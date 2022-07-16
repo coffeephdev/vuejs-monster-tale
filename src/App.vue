9d449ae669b6b9fdd2a4f42ff052460623898c5b
p.houot <pierre.houot@dont-nod.com>
2022-07-16 17:50:16 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template lang="pug">
+.main-wrapper
+  
+  RouterView(class="main-container")
+</template>
+
+<style scoped>
+.main-container {
+  width: 70vw;
+  height: calc(70vw / 16 * 9);
+  border-radius: 1em;
+  box-shadow: 5px 2px 15px 0 black;
+
+  font-weight: normal;
+}
+.main-wrapper {
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BigButton from '@/components/BigButton.vue'
+</script>
+
+<template lang="pug">
+.welcome
+  big-button Entrer
+</template>
+
+<style>
+.welcome {
+  background: no-repeat url(../assets/img/monster-tale.png);
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
